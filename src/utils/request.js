745e3381e8b6f84a2728b168a487c079b8334dfa
KelvinYuan-co <166618517+kelvin-yyy@users.noreply.github.com>
2024-04-11 06:40:22 +0800

    Update request.js

--- src/utils/request.js
+++ src/utils/request.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 // create an axios instance
 const service = axios.create({
   // baseURL: "https://dev-admin.soulsmint.com/api", // 服务器
-  baseURL: "https://avenso.xyz",
+  baseURL: "https://www.avenso.xyz",
   timeout: 50000 // request timeout
 })
 
